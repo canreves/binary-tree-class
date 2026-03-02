aa75c55bb56caadeee4cae39ee75418a1c3560f1
Can Sever <cansever1005@gmail.com>
2026-03-02 18:53:43 +0300

    added insert function

--- bstree.cpp
+++ bstree.cpp
@@ -20,5 +20,39 @@ bstree::~bstree(){
     }
 }
 
+void bstree::insert(int num){
+    if(!root){
+            root = new node(num);
+            return;
+        }
+
+        node* ptr1 = root;
+        node* ptr2;
+
+        while(ptr1){
+            if(num > ptr1->info){
+                ptr2 = ptr1;
+                ptr1 = ptr1->right;
+            }
+            else if(num < ptr1->info){
+                ptr2 = ptr1;
+                ptr1 = ptr1->left;
+            }
+
+            else{
+                cout<<"Duplicate node error!"<<endl;
+                return;
+            }
+        }
+        
+        if(ptr2->info > num){
+            ptr2->left = new node(num);
+        }
+
+        else if(ptr2->info < num){
+            ptr2->right = new node(num);
+        }
+}
+
 
 
